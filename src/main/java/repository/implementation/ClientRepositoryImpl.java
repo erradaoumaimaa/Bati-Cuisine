586d240b3866c3d6eac824eaaa039fa80e129968
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-09-21 23:43:48 +0100

    Add utility methods for parsing integers and validating email format

---
+++ src/main/java/repository/implementation/ClientRepositoryImpl.java
@@ -0,0 +1,4 @@
+package repository.implementation;
+
+public class ClientRepositoryImpl {
+}
---
+++ src/main/java/util/tools.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class tools {
+    // return 0 =>  not a valid integer
+    public static Integer tryParse(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+    // Return true => the email is valid || false if not
+    public static boolean isValidEmailFormat(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return !matcher.matches();
+    }
+
+
+}
