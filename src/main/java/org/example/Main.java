707ed974311861098c4cbaf8fd0796ba481077ee
ER-RADA Oumaima <erradaoumaima@gmail.com>
2024-09-22 15:40:56 +0100

    BAT-27 Tester l'ajout de clients dans la base de données

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,24 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
+        // Récupération de la connexion via Optional
+        Optional<Connection> optionalConnection = JdbcConnection.getConnection();
+
+        // Test de la connexion si presente
+        if (optionalConnection.isPresent()) {
+            try (Connection connection = optionalConnection.get()) {
+                // Initialisation des repositories et services
+                ClientRepositoryImpl clientRepository = new ClientRepositoryImpl(connection);
+                ClientServiceImpl clientService = new ClientServiceImpl(clientRepository);
+                ClientController clientController = new ClientController(clientService);
+
+                // méthode ajouter un client
+                clientController.ajouterClient();
+            } catch (Exception e) {
+                System.out.println("Erreur lors de l'utilisation de la connexion : " + e.getMessage());
+            }
+        } else {
+            System.out.println("Erreur : Connexion non disponible.");
+        }
     }
 }
